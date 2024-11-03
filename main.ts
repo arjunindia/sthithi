c016179ca139496bc0784ad8eb9cf6d83e7a965f
arjunindia <arjun8107@gmail.com>
2024-11-03 09:12:04 +0000

    Better comments

--- main.ts
+++ main.ts
@@ -1,65 +1,115 @@
 import { useSyncExternalStore } from "npm:react@^18.3.1";
 
 class State<T> {
+  /** The current state of the store */
   private state: T;
+  /** The subscribers to the store
+   *  A subscriber is a function that is recalled when the state changes so that it now reflects the new state
+   */
   private subscribers: Set<(state?: T) => void>;
+  /** The effects to be run when the state changes
+   *  An effect is a function that is called when one or more of the dependent states change
+   */
   private effects: Set<(state?: T) => void>;
+
   constructor(initialState: T) {
     this.state = initialState;
     this.subscribers = new Set();
     this.effects = new Set();
   }
 
-  get() {
+  /** Get the current state of the store. This does not trigger re-renders. */
+  get(): T {
     return this.state;
   }
 
+  /** Set the current state of the store. This does not trigger re-renders. */
   set(newState: T) {
     this.state = newState;
   }
 
-  addSubscriber(subscriber: () => void) {
+  /** Add a function that subscribes to the store. */
+  addSubscriber(subscriber: () => void): void {
     this.subscribers.add(subscriber);
   }
 
-  removeSubscriber(subscriber: () => void) {
+  /** Remove a function that subscribes to the store. */
+  removeSubscriber(subscriber: () => void): void {
     this.subscribers.delete(subscriber);
   }
 
-  addEffect(effect: () => void) {
+  /** Add a side-effect to the store. */
+  addEffect(effect: () => void): void {
     this.effects.add(effect);
   }
 
-  removeEffect(effect: () => void) {
+  /** Remove a side-effect from the store. */
+  removeEffect(effect: () => void): void {
     this.effects.delete(effect);
   }
 
-  runEffects() {
+  /** Run all the effects that depend on the current state. */
+  runEffects(): void {
     this.effects.forEach((effect) => effect());
   }
-  updateSubscribers() {
+
+  /** Run and update all the subscribers. */
+  updateSubscribers(): void {
     this.subscribers.forEach((subscriber) => subscriber());
   }
 }
 
-export const createState = <T>(initialState: T) => {
+/** Create a new global state object. This is the main entry point for creating a state object.
+ * @param initialState The initial state of the store.
+ * @returns A state object that can be used to access the current state and subscribe to changes.
+ */
+export const createState = <T>(initialState: T): State<T> => {
   const state = new State<T>(initialState);
   return state;
 };
 
-export const createEffect = <T>(effect: () => void, deps: State<T>[]) => {
+/** Create a new effect. This is the main entry point for creating a side-effect.
+ * @param effect The side-effect to be run when the state changes.
+ * @param deps The dependent states that the effect depends on.
+ * @returns A function that removes the effect from the store.
+ */
+export const createEffect = <T>(
+  effect: () => void,
+  deps: State<T>[],
+): () => void => {
   deps.forEach((dep) => dep.addEffect(effect));
   return () => {
     deps.forEach((dep) => dep.removeEffect(effect));
   };
 };
 
-export const dispatch = <T>(state: State<T>, updater: (state: T) => T) => {
+/**
+ * Dispatch a new state to the store. This will trigger a re-render of all subscribers.
+ * This function does not subscribe to the store. The use for this is to update the state from outside the component, from a component that is not subscribed to the store.
+ * This helps avoid unnecessary re-renders.
+ * The updater function is called with the current state and should return the new state.
+ * @param state The state object to dispatch to.
+ * @param updater The updater function that takes the current state and returns the new state.
+ * @example
+ * import { createState, dispatch } from "sthithi";
+ * const state = createState(0);
+ * const increment = () => dispatch(state, (prevState) => prevState + 1);
+ * const decrement = () => dispatch(state, (prevState) => prevState - 1);
+ */
+export const dispatch = <T>(
+  state: State<T>,
+  updater: (state: T) => T,
+): void => {
   state.set(updater(state.get()));
   state.updateSubscribers();
   state.runEffects();
 };
 
+/** useLibState is a custom hook that allows you to use the state object in a React component.
+ * It returns a tuple of the current state and a function that dispatches a new state to the store.
+ * @param state The state object to use.
+ * @returns A tuple of the current state and a function that dispatches a new state to the store.
+ */
 export const useLibState = <T>(
   state: State<T>,
 ): readonly [T, (state: T) => void] => {
